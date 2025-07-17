874cc85feaf60d72fb194d7b5c0ff49f3f020cea
Vi Nguyen <nguyenvv13@gmail.com>
2025-07-17 17:30:56 -0400

    Adding path parameters and queries

--- example-express/express-ex.js
+++ example-express/express-ex.js
@@ -13,5 +13,24 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
+// Adding another path
+app.get('/example', (req, res) => {
+    res.send('Hitting the Example path');
+});
+
+// Path Parameters
+app.get('/example/:name/:age', (req, res) => {
+    // In this case, we specified that the two inputs after the /example path are
+    // parameters `name` and `age`, and both parameters MUST be present to work.
+    // Parameters should be used when the data is REQUIRED.
+    console.log(req.params);
+    res.send(req.params.name + ": " + req.params.age);
+    
+    // Queries are specified in the URL as `/...?key=value`
+    // More queries can be added using `&`, like `/...?key1=value1&key2=value2`
+    // Queries should be used for optional settings.
+    console.log(req.query);
+});
+
 // Like Gets the server up and running by telling the server to listen to port 3000
 app.listen(3000);
