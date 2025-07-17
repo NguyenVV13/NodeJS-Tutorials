a5cb037e7ae56d138a2be890e2d50a4c0c50f3b7
Vi Nguyen <nguyenvv13@gmail.com>
2025-07-17 01:46:10 -0400

    Adding package file and dependencies

--- example-server/http-server.js
+++ example-server/http-server.js
@@ -7,18 +7,17 @@ const fs = require('fs');  // Filesystem for serving static files
 
 
 
+// Example showing use of package that was installed with npm
+const lodash = require('lodash');
+let ex = lodash.fill([1,2,3,4,5], 'banana', 1, 4);
+console.log(ex);
+
+
+
 // req is what the client requests from this server
 // res is what this server responds to the client with
 const server = http.createServer((req, res) => {
-    // const readStream = fs.createReadStream('./files/example.txt');
-    // res.writeHead(200, {'content-type': 'text/plain'})  // Response header
-    // readStream.pipe(res);  // Pipe content from the filesystem readstream into the response
-
-    const readStream = fs.createReadStream('./files/orange.png');
-    res.writeHead(200, {'content-type': 'image/png'})  // Response header
-    readStream.pipe(res);  // Pipe content from the filesystem readstream into the response
-
-
+    /* Basic path logic */
     // if(req.url === '/') {
     //     // http://localhost:port/
     //     res.write('Hello World from Node.js at root directory');  // Sets the response
@@ -29,6 +28,16 @@ const server = http.createServer((req, res) => {
     //     res.write('Message from some other domain');
     //     res.end();
     // }
+
+    /* Serving a static file */
+    // const readStream = fs.createReadStream('./example.txt');
+    // res.writeHead(200, {'content-type': 'text/plain'})  // Response header
+    // readStream.pipe(res);  // Pipe content from the filesystem readstream into the response
+
+    const readStream = fs.createReadStream('./orange.png');
+    res.writeHead(200, {'content-type': 'image/png'})  // Response header
+    readStream.pipe(res);  // Pipe content from the filesystem readstream into the response
+
 });
 
 // Gets the server up and running by telling the server to listen to port 3000
